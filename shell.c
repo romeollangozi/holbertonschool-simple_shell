0833adb4c449d579637ba523191b26d2ed23a7d6
romeollangozi <romeollangozi123@gmail.com>
2023-08-17 02:19:39 -0700

    shell

--- shell.c
+++ shell.c
@@ -5,19 +5,19 @@ int main()
         char *commandLine;
         char **argum;
 	int chars;
-	size_t max = MAX_CHARS;
-
+	size_t max = MAX_CHARS;	
         while (1)
         {
-		
-		printf("$");
+		if (isatty(isatty(STDIN_FILENO)))
+			printf("$");
+
                 chars = getline(&commandLine, &max, stdin);
                 if (strcmp(commandLine, "\n") == 0)
                         continue;
                 if (strcmp(commandLine, "exit\n") == 0)
                         break;
 		if (chars == -1)
-			break;
+			return(1);
 
 		argum = token_line(commandLine, " \n");
 		if (strcmp("cd", argum[0]) == 0)
@@ -31,14 +31,16 @@ int main()
                               if(execvp(argum[0], argum))
 			      {
 					printf("%s: command not found\n", argum[0]);
-					continue;
+					return (1);
 			      }
                 }
                 else
                 {
                         wait(NULL);
 			free(argum);
+			continue;
                 }
+		break;
       }
 	return (0);
 }
